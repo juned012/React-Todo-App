a9425db05e7bb541680ec1a02d465d95f125f617
juned072 <junedansari9172@gmail.com>
2025-01-01 11:06:04 +0530

    Added a icons before links text

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,16 @@
+import { GoTasklist } from "react-icons/go";
+import { MdTaskAlt } from "react-icons/md";
+import { IoIosTimer } from "react-icons/io";
+
 const NavBar = () => {
   return (
     <div className="pt-2 pb-8">
       <div className="flex gap-5">
         <div className="relative cursor-pointer text-gray-800 font-semibold group">
-          <span>All Tasks</span>
+          <p className="flex items-center gap-1">
+            <GoTasklist />
+            <span>All Tasks</span>
+          </p>
           <span
             className="absolute bottom-[-4px] left-0 w-full h-0.5 bg-green-600 transform 
     scale-x-0 group-hover:scale-x-100 transition-transform duration-300"
@@ -11,14 +18,21 @@ const NavBar = () => {
         </div>
 
         <div className="relative cursor-pointer text-gray-800 group">
-          <span>Recently Added</span>
+          <p className="flex items-center gap-1">
+            <IoIosTimer />
+            <span>Recently Added</span>
+          </p>
           <span
             className="absolute bottom-[-4px] left-0 w-full h-0.5 bg-green-600 transform 
           scale-x-0 group-hover:scale-x-100 transition-transform duration-300"
           ></span>
         </div>
         <div className="relative cursor-pointer text-gray-800 group">
-          <span>Completed</span>
+          <p className="flex items-center gap-1">
+            <MdTaskAlt />
+            <span>Completed</span>
+          </p>
+
           <span
             className="absolute bottom-[-4px] left-0 w-full h-0.5 bg-green-600 transform 
           scale-x-0 group-hover:scale-x-100 transition-transform duration-300"
