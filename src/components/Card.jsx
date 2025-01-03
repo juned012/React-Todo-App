271ee9dad976def2ff90a5a0953c8a0e417e09ed
juned012 <junedansari9172@gmail.com>
2025-01-03 09:34:52 +0530

    Added a text for when task is not added then it will be show

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,17 +6,23 @@ const Card = () => {
 
   return (
     <>
-      {todoData.map((todo, index) => (
-        <div
-          key={index}
-          className="shadow-md border border-gray-100 bg-white p-4 rounded-xl cursor-pointer"
-        >
-          <div>
-            <h1 className="text-xl font-semibold pb-2">{todo.title}</h1>
-            <p className="text-sm">{todo.description}</p>
-          </div>
-        </div>
-      ))}
+      {todoData.length === 0 ? (
+        <h2>No Tasks!</h2>
+      ) : (
+        <>
+          {todoData.map((todo, index) => (
+            <div
+              key={index}
+              className="shadow-md border border-gray-100 bg-white p-4 rounded-xl cursor-pointer"
+            >
+              <div>
+                <h1 className="text-xl font-semibold pb-2">{todo.title}</h1>
+                <p className="text-sm">{todo.description}</p>
+              </div>
+            </div>
+          ))}
+        </>
+      )}
     </>
   );
 };
