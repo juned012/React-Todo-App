d183092860a27be1f634e110b9f7b59fb1b0d83f
juned012 <junedansari9172@gmail.com>
2025-01-03 09:29:32 +0530

    Map todo data to dynamic cards Data show

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,23 @@
+import { useContext } from "react";
+import { TodoContext } from "../context/TodoProvider";
+
 const Card = () => {
+  const { todoData } = useContext(TodoContext);
+
   return (
-    <div className="shadow-md border border-gray-100 bg-white p-4 rounded-xl cursor-pointer">
-      <div>
-        <h1 className="text-xl font-semibold pb-2">
-          Lorem ipsum dolor sit amet.
-        </h1>
-        <p className="text-sm">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga esse
-          quidem, atque praesentium dolore excepturi quia, voluptatum voluptatem
-          alias, eos minima necessitatibus et aperiam quisquam earum harum
-          dignissimos nostrum.
-        </p>
-      </div>
-    </div>
+    <>
+      {todoData.map((todo, index) => (
+        <div
+          key={index}
+          className="shadow-md border border-gray-100 bg-white p-4 rounded-xl cursor-pointer"
+        >
+          <div>
+            <h1 className="text-xl font-semibold pb-2">{todo.title}</h1>
+            <p className="text-sm">{todo.description}</p>
+          </div>
+        </div>
+      ))}
+    </>
   );
 };
 
